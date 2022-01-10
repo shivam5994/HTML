21b31973e212839cbe83e3baa7f513840ca97792
shivam5994 <56692355+shivam5994@users.noreply.github.com>
2022-01-10 13:12:20 +0530

    Update main.js

--- PSD to HTML/assets/js/main.js
+++ PSD to HTML/assets/js/main.js
@@ -14,3 +14,65 @@ document.addEventListener("scroll", () => {
 function privacy_policy_btn() {
     $(".privacy-policy-sec").css("display", "none");
 }
+
+var open = false;
+let openSideBar = () => {
+
+    if (!open) {
+        open = true;
+        document.getElementById("sidebar").classList.add("active");
+        document.getElementById("navbarSupportedContent").remove("show");
+    } else {
+        open = false;
+        document.getElementById("sidebar").classList.remove("active");
+    }
+
+}
+
+let changeAvatar = (e) => {
+    let all = document.getElementsByClassName("selected");
+    let activeAvatar = document.getElementById("active-avatar");
+    for (let i = 0; i <= all.length; i++) {
+        if (all[i].classList.contains("active")) {
+            all[i].classList.remove("active");
+            e.target.classList.add("active");
+            activeAvatar.src = e.target.src;
+        } else {
+            continue;
+        }
+    }
+}
+
+let removeActive = (e) => {
+    let all = document.getElementsByClassName("dropdown-item");
+    for (let i = 0; i <= all.length; i++) {
+        if (all[i].classList.contains("active")) {
+            all[i].classList.remove("active");
+            e.target.classList.add("active");
+            e.target.classList.toggle("active");
+        } else {
+            continue;
+        }
+    }
+}
+
+let closeSideBar = () => {
+    open = false;
+    document.getElementById("sidebar").classList.remove("active");
+}
+
+
+function initMap() {
+    // The location of Uluru
+    const uluru = { lat: 51.165691, lng: 10.451526 };
+    // The map, centered at Uluru
+    const map = new google.maps.Map(document.getElementById("map"), {
+        zoom: 9,
+        center: uluru,
+    });
+    // The marker, positioned at Uluru
+    const marker = new google.maps.Marker({
+        position: uluru,
+        map: map,
+    });
+}
